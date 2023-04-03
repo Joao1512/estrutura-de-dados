3199dfa11c76e575633c03a1cf01d0ba1714427c
Joao1512 <jvictorsimao@hotmail.com>
2023-04-03 17:01:57 -0300

    ponteiro e Struct - corrigindo prints

--- ponteiroEStruct.c
+++ ponteiroEStruct.c
@@ -10,11 +10,12 @@ typedef struct {
 int main() {
     int x = 25;
     int* y = &x;
-    printf("Valor de x: %i, Endereco de x na memoria: %i", x, y);
+    printf("-Valor de x: %i, Endereco de x na memoria: %i", x, y);
 
     Pessoa* enderecoPessoa =  (Pessoa*) malloc(sizeof(Pessoa));
+    printf("\nEndereco alocado para pessoa e: %i", enderecoPessoa);
     enderecoPessoa->altura = 1;
     enderecoPessoa->peso = 90;
 
-    printf("Endereco de Pessoa: %i, tamanho de alocacao: %i altura: %i", enderecoPessoa, sizeof(Pessoa), enderecoPessoa->altura);
+    printf("\n-Endereco de Pessoa: %i, tamanho de alocacao: %i altura: %i", enderecoPessoa, sizeof(Pessoa), enderecoPessoa->altura);
 }
